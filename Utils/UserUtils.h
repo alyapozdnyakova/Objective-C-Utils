03a5a1c93c34ab9b81e45abb400804e802700f83
Alexandra Pozdnyakova <ylibatsya@gmail.com>
2014-07-30 23:58:26 +0400

    User Utils

---
+++ Utils/UserUtils.h
@@ -0,0 +1,13 @@
+//
+//  UserUtils.h
+//  Objective-C-Utils
+//
+//  Created by Alexandra Pozdnyakova on 30/07/2014.
+//  Copyright (c) 2014 Alexandra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserUtils : NSObject
+
+@end
---
+++ Utils/UserUtils.m
@@ -0,0 +1,14 @@
+//
+//  UserUtils.m
+//  Objective-C-Utils
+//
+//  Created by Alexandra Pozdnyakova on 30/07/2014.
+//  Copyright (c) 2014 Alexandra. All rights reserved.
+//
+
+#import "UserUtils.h"
+
+@implementation UserUtils
+
+
+@end
